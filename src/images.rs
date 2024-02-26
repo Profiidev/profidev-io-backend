9a33b4e12c7c34e89a25cf168475892cc549dc71
Profiidev <92174452+Profiidev@users.noreply.github.com>
2024-02-26 16:33:33 +0000

    Added mediatype to apod

--- src/images.rs
+++ src/images.rs
@@ -1,14 +1,16 @@
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use tide::Request;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct APODRes {
   url: String,
+  media_type: String,
 }
 
 pub(crate) async fn apod(_req: Request<()>) -> tide::Result {
   let url = format!("https://api.nasa.gov/planetary/apod?api_key={}", *crate::NASA_API_KEY);
   let mut res = surf::get(url).await?;
   let apod: APODRes = res.body_json().await?;
-  Ok(apod.url.into())
+  let res = tide::Response::builder(200).body(tide::Body::from_json(&apod)?);
+  Ok(res.build())
 }
